810c960843f419f7aea362baacfd2caaf351d4a2
kky0426 <ksakdlf99@naver.com>
2016-04-03 16:53:47 +0900

    rock scissors paper

---
+++ w5main2.py
@@ -0,0 +1,20 @@
+﻿userA=raw_input("userA input r,s,p:")
+userB=raw_input("userB input r,s,p:")
+if userA==userB:
+    print "draw"
+elif userA=='r':
+    if userB=='s':
+        print "userA win"
+    elif userB=='p':
+        print "userB win"
+elif userA=='s':
+    if userB=='r':
+        print "userB win"
+    elif userB=='p':
+        print "userA win"
+elif userA=='p':
+    if userB=='r':
+        print "userA win"
+    elif userB=='s':
+        print "userB win"
+    
