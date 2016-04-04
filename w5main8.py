d989ba4c12c2a8aade0bd1bd30df6425c10c29e5
kky0426 <ksakdlf99@naver.com>
2016-04-04 20:16:44 +0900

    BMI

---
+++ w5main8.py
@@ -0,0 +1,23 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def computeBMI():
+    sel1=raw_input("input your weight:")
+    sel2=raw_input("input your height:")
+
+    bmi=float(sel1)/(float(sel2)*float(sel2))
+
+    if bmi>=18.5 and bmi<25.0:
+      print "nomal weight"
+    elif bmi>=25.0 and bmi<=29.9:
+      print "over weight"
+    else:
+       print "error"
+computeBMI()
+def lab5():
+    computeBMI()
+def main():
+    lab5()
+wn.exitonclick()
+if __name__=="__main__":
+    main()	
