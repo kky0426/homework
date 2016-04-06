5f647398a54d9803ff60fdb81b500731b7e135a1
kky0426 <ksakdlf99@naver.com>
2016-04-06 15:45:53 +0900

    A leap year

---
+++ w6main11.py
@@ -0,0 +1,16 @@
+﻿def aLeapyear():
+    sel=raw_input("input year:")
+    if float(sel)%4==0 and (float(sel)%100!=0 or float(sel)%400==0):
+        print "a leap year"
+    else:
+        print "No"
+
+def lab6():
+	aLeapyear()
+def main():
+	lab6()    
+
+
+
+if __name__=="__main__":
+    main()
