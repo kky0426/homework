4a1c82a8652afb353018c02dacee4ced672040a7
kky0426 <ksakdlf99@naver.com>
2016-04-11 13:53:36 +0900

    윤년,리스트

---
+++ w6main.py
@@ -0,0 +1,36 @@
+﻿def aLeapyear():
+    sel=raw_input("input year:")
+    if float(sel)%4==0 and (float(sel)%100!=0 or float(sel)%400==0):
+        print "a leap year"
+    else:
+        print "No"
+
+
+
+def sumList(aList):
+    x=list()
+    sum=0
+    for i in range(0,aList):
+        if(i%4==0 and i%5):
+            x.append(i)
+    for i in range(0,len(x)):
+        sum+=x[i]
+    return sum
+
+
+
+
+def lab6-2():
+	sumList(2000)
+
+
+
+def lab6-1():
+	aLeapyear()
+def main():
+	lab6-1()    
+	lab6-2()
+
+
+if __name__=="__main__":
+    main()
