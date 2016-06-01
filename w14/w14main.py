7d6a5fe8a0c35e16b6eac91a25b23486f0cb5c61
kky0426 <ksakdlf99@naver.com>
2016-06-01 20:02:54 +0900

    객체지향

---
+++ w14/w14main.py
@@ -0,0 +1,34 @@
+﻿
+
+class Dog(object):   
+    def talk(self):
+        print "mung mung"
+
+class ShihTzuDog(object):
+    def talk(self):
+        print "si si"
+        
+class Maltese(object):
+    def talk(self):
+        print "mal mal"
+
+def DogTalk():
+	mydog1=Dog()
+	mydog1.talk()
+	mydog2=Maltese()
+	mydog2.talk()
+	mydog3=ShihTzuDog()
+	mydog3.talk()
+
+
+
+def lab14():
+	DogTalk()
+
+
+def main():
+	lab14()
+
+
+if __name__=="__main__": 
+     main()
